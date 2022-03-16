ee017359417185d7078f8ce6f4ffb9d090098c99
Lucian Ghinescu <ghinesculucian20@gmail.com>
2022-03-16 14:09:02 +0200

    Exceptions and debugging assignment

---
+++ Modulul 1/4. Exception handling and debugging/Ghini-Bikes/Products/Bike.cs
@@ -0,0 +1,43 @@
+﻿using Ghini_Bikes.Models;
+using System;
+
+namespace Ghini_Bikes.Products
+{
+
+	/// <summary>
+	/// Summary description for Class1
+	/// </summary>
+	public class Bike:Product
+	{
+		private static int lastId = 0;
+		private readonly int id;
+		
+		private int year;
+		private string specs;
+
+		public int Year { get { return year; } set { year = value; } }
+		public string Specification { get { return specs; } set { specs = value; } }
+
+		public Bike(string man, string model, int year , string specs,double price)
+		{
+			Manufacturer = man;
+			Model	 = model;
+			Year = year;
+			Specification = specs;
+			Price = price;
+			id= lastId++;
+		}
+
+		public override string ToString()
+        {
+			string s = "Normal bike!";
+			return s;
+        }
+		public double Coeficient()
+        {
+			double coef = Price/this.year;
+			return coef;
+
+        }
+	}
+}
---
+++ Modulul 1/4. Exception handling and debugging/Ghini-Bikes/Products/Part.cs
@@ -0,0 +1,14 @@
+﻿using Ghini_Bikes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ghini_Bikes.Products
+{
+    public class Part : Product
+    {
+
+    }
+}

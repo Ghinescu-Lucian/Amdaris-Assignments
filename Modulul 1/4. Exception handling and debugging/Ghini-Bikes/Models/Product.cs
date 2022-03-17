b1727a832c5d76cd0077507570cf03e67b96e578
Lucian Ghinescu <ghinesculucian20@gmail.com>
2022-03-17 22:00:31 +0200

    Arrays, Collections and Generic assignment

---
+++ Modulul 1/4. Exception handling and debugging/Ghini-Bikes/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ghini_Bikes.Models
+{
+    public abstract class Product
+    {
+        private string manufacturer;
+        private string model;
+        private double price;
+        public double Price { get { return price; } set { price = value; } }
+        public string Manufacturer { get { return manufacturer; } set { manufacturer = value; } }
+        public string Model { get { return model; } set { model = value; } }
+
+        public override string ToString()
+        {
+            string s = manufacturer + " " + model + " " + price;
+            return s;
+        }
+    }
+}
